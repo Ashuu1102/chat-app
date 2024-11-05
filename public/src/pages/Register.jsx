b6e6fdb7190b5b775ec563346b00ccdac24c898e
Ashutosh <aa8781@srmist.edu.in>
2024-11-05 21:25:33 +0530

    Add login functionality

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -25,6 +25,11 @@ function Register() {
         draggable: true,
         theme: "dark"
     }
+    useEffect(() => {
+        if(localStorage.getItem('chat-app-user')){
+            navigate("/")
+        }
+    }, [])
     const handleSubmit = async (event) => {
         event.preventDefault()
         if(handleValidation()){
