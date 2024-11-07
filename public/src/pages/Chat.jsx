78ddff495a7def6ea5802f852aa513982530bd95
Ashutosh <aa8781@srmist.edu.in>
2024-11-07 18:13:12 +0530

    Chat header and logout button

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -5,12 +5,14 @@ import { useNavigate } from 'react-router-dom'
 import { allUsersRoute } from '../utils/APIRoutes'
 import Contacts from '../components/Contacts'
 import Welcome from '../components/Welcome'
+import ChatContainer from '../components/ChatContainer'
 
 function Chat() {
 
   const [contacts, setContacts] = useState([])
   const [currentUser, setCurrentUser] = useState(undefined)
  const [currentChat, setCurrentChat] = useState(undefined)
+ const [isLoaded, setIsLoaded] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -19,6 +21,7 @@ function Chat() {
       navigate("/login")
     }else{
       setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+      setIsLoaded(true)
     }}
     chat()
   }, [])
@@ -45,7 +48,11 @@ function Chat() {
       currentUser = {currentUser} 
       changeChat = {handleChatChange} 
       />
-      <Welcome currentUser = {currentUser} />
+      {isLoaded && currentChat === undefined ? (
+        <Welcome currentUser = {currentUser} />
+      ): (
+        <ChatContainer currentChat = {currentChat} />
+      )}
     </div>
   </Container>
   )
